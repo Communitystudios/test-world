99fb0740a31c38ecb9ca307dcdebec8147c3c1a2
Communitystudios <66947121+Communitystudios@users.noreply.github.com>
2020-06-15 16:27:08 +0600

    Add files via upload

---
+++ EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public Vector3 teleportPosition;
+    public GameObject delete;
+    void OnTriggerEnter(Collider other)
+    {          
+        if (other.tag == "Untagged")
+        {
+            transform.position = teleportPosition;
+        }
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("test");
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");
+    }
+}
